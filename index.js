c70d0a57ec69ba54ef1a06152f896e394e30c784
Nathan Chuluda <nchuluda@gmail.com>
2023-06-15 19:57:50 -0400

    3.16

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
+require('dotenv').config()
+const cors = require('cors')
 const express = require('express')
 var morgan = require('morgan')
 
+const Person = require('./models/person')
+
 const app = express()
 
-const cors = require('cors')
 app.use(cors())
-
 app.use(express.static('build'))
 app.use(express.json())
 app.use(morgan(':method :url :res[content-length] - :response-time ms :body'))
@@ -14,97 +16,97 @@ morgan.token('body', req => {
     return JSON.stringify(req.body)
   })
 
-let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
 
-app.get('/info', (request, response) => {
-    let personCount = persons.length
-    let date = Date()
-    response.send(`<p>Phonebook has info for ${personCount} people</p><p>${date}</p>`)
-  })
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+let persons = []
+
+// app.get('/info', (request, response) => {
+//     let personCount = persons.length
+//     let date = Date()
+//     response.send(`<p>Phonebook has info for ${personCount} people</p><p>${date}</p>`)
+//   })
 
 app.get('/api/persons', (request, response) => {
+  Person.find({}).then(persons => {
     response.json(persons)
   })
-
-app.get('/api/persons/:id', (request, response) => {
-const id = Number(request.params.id)
-const person = persons.find(person => person.id === id)
-
-if (person) {
-    response.json(person)
-} else {
-    response.status(404).end()
-}
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
-  
-    response.status(204).end()
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+  })
+  .catch(error => next(error))
 })
+
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
 
 app.post('/api/persons', (request, response) => {
-    const body = request.body
-  
-    if (!body.name) {
-      return response.status(400).json({ 
-        error: 'name missing' 
-        })
-    }
+  const body = request.body
 
-    if (!body.number) {
-        return response.status(400).json({ 
-          error: 'number missing' 
-        })
-    }
+  if (body.name === undefined) {
+    return response.status(400).json({ error: 'name missing' })
+  }
 
-    if (persons.find(person => person.name === body.name)) {
-        return response.status(400).json({
-            error: 'name already exists in phonebook'
-        })
-    }
-  
-    const person = {
-      id: Math.floor(Math.random() * 10000),
-      name: body.name,
-      number: body.number
-    }
-  
-    persons = persons.concat(person)
+  if (body.number === undefined) {
+    return response.status(400).json({ error: 'number missing' })
+  }
 
+  const person = new Person({
+    name: body.name,
+    number: body.number
+  })
 
-    response.json(person)
+  person.save().then(savedPerson => {
+    response.json(savedPerson)
+  })
 })
 
-const unknownEndpoint = (request, response) => {
-    response.status(404).send({ error: 'unknown endpoint' })
-  }
-  
-app.use(unknownEndpoint)
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
 
-const PORT = process.env.PORT || 3001
+  const person = {
+    name: body.name,
+    number: body.number
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
+})
+
+
+
+app.use(unknownEndpoint)
+app.use(errorHandler)
+
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
---
+++ mongo.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length<3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url =
+  `mongodb+srv://fullstack:${password}@fullstackopen.gxpop.mongodb.net/phonebookApp?retryWrites=true&w=majority`
+
+mongoose.set('strictQuery',false)
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+// Add new entry to phonebook
+if (process.argv.length === 5) {
+    const name = process.argv[3]
+    const number = process.argv[4]
+
+    const person = new Person({
+        name: `${name}`,
+        number: `${number}`,
+    })
+
+    person.save().then(result => {
+        console.log('person saved!')
+        mongoose.connection.close()
+    })
+}
+
+// Display entries in phonebook
+if (process.argv.length === 3) {
+    Person.find({}).then(result => {
+        console.log('Phonebook:')
+        result.forEach(person => {
+          console.log(`${person.name} ${person.number}`)
+        })
+        mongoose.connection.close()
+      })
+}
+
