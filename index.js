102760aa1eb05211e7e11f9699c3c40142d4638f
Nathan Chuluda <nchuluda@gmail.com>
2023-06-16 11:04:20 -0400

    3.18

--- index.js
+++ index.js
@@ -25,18 +25,21 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
-  } 
-
+  } else if (error.name = 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
   next(error)
 }
 
 let persons = []
 
-// app.get('/info', (request, response) => {
-//     let personCount = persons.length
-//     let date = Date()
-//     response.send(`<p>Phonebook has info for ${personCount} people</p><p>${date}</p>`)
-//   })
+app.get('/info', (request, response) => {
+    Person.countDocuments({}, { hint: "_id_" }).then(res => {
+      const count = res
+      let date = Date()
+      response.send(`<p>Phonebook has info for ${count} people</p><p>${date}</p>`)
+    })
+  })
 
 app.get('/api/persons', (request, response) => {
   Person.find({}).then(persons => {
@@ -65,16 +68,16 @@ app.delete('/api/persons/:id', (request, response, next) => {
 })
 
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
   const body = request.body
 
-  if (body.name === undefined) {
-    return response.status(400).json({ error: 'name missing' })
-  }
+  // if (body.name === undefined) {
+  //   return response.status(400).json({ error: 'name missing' })
+  // }
 
-  if (body.number === undefined) {
-    return response.status(400).json({ error: 'number missing' })
-  }
+  // if (body.number === undefined) {
+  //   return response.status(400).json({ error: 'number missing' })
+  // }
 
   const person = new Person({
     name: body.name,
@@ -84,24 +87,22 @@ app.post('/api/persons', (request, response) => {
   person.save().then(savedPerson => {
     response.json(savedPerson)
   })
+  .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
-  const body = request.body
+  const { name, number } = request.body
 
-  const person = {
-    name: body.name,
-    number: body.number
-  }
-
-  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+  Person.findByIdAndUpdate(
+    request.params.id, 
+    { name, number },
+    { new: true, runValidators: true, context: 'query' }
+  ) 
     .then(updatedPerson => {
       response.json(updatedPerson)
     })
     .catch(error => next(error))
 })
-
-
 
 app.use(unknownEndpoint)
 app.use(errorHandler)
--- models/person.js
+++ models/person.js
@@ -15,8 +15,8 @@ mongoose.connect(url)
   })
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
+  name: {type: String, required: true},
+  number: {type: String, required: true}
 })
 
 personSchema.set('toJSON', {
